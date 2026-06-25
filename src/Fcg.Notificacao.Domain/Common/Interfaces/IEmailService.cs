5a5b38dae15102e915c97a7fe8d19b3adb1111d4
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-24 23:23:23 -0300

    feat(notifications): implementacao do servico de email fake e consumer de boas-vindas

---
+++ src/Fcg.Notificacao.Domain/Common/Interfaces/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace Fcg.Notificacao.Domain.Common.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendWelcomeEmailAsync(Guid UsuarioId, string Nome, string Email);
+    }
+}
