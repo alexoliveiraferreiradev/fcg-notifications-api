2d9ad1e9d3334cbace928b869dc24d8426241bb4
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-29 22:09:19 -0300

    feat(api): adicionar consumer para evento de pagamento processado

---
+++ src/Fcg.Notificacao.API/Consumers/PaymentProcessedEventConsumer.cs
@@ -0,0 +1,13 @@
+﻿using Fcg.Core.Abstractions.MessageContracts;
+using MassTransit;
+
+namespace Fcg.Notificacao.API.Consumers
+{
+    public class PaymentProcessedEventConsumer : IConsumer<PaymentProcessedEvent>
+    {
+        public Task Consume(ConsumeContext<PaymentProcessedEvent> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
