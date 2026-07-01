14ca3fb3aca0ea15bd5b116725beddec117331cf
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-01 16:58:57 -0300

    feat: implementar Clean Architecture e UseCases de envio de e-mail na Application

---
+++ src/Fcg.Notificacao.Application/Ports/IIdempotencyService.cs
@@ -0,0 +1,8 @@
+﻿namespace Fcg.Notificacao.Application.Ports
+{
+    public interface IIdempotencyService
+    {
+        Task<bool> HasBeenProcessedAsync(Guid eventId);
+        Task MarkAsProcessedAsync(Guid eventId);
+    }
+}
---
+++ src/Fcg.Notificacao.Application/UseCase/WelcomeEmail/SendWelcomeEmailCommand.cs
@@ -0,0 +1,4 @@
+﻿namespace Fcg.Notificacao.Application.UseCase.WelcomeEmail
+{
+    public record SendWelcomeEmailCommand(Guid EventId, Guid UserId, string Email, string UserName);
+}
