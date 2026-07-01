9c19a0f686307f9a3e066803600d78daca6e540f
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-01 16:58:40 -0300

    feat: adicionar entidades de Notificacao e corrigir typo do ValueObject de Email

---
+++ src/Fcg.Notificacao.Domain/Enum/NotificationStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Fcg.Notificacao.Domain.Enum
+{
+    public enum NotificationStatus
+    {
+        Pending = 1,
+        Sent = 2,
+        Failed = 3
+    }
+}
---
+++ src/Fcg.Notificacao.Domain/Enum/NotificationType.cs
@@ -0,0 +1,8 @@
+﻿namespace Fcg.Notificacao.Domain.Enum
+{
+    public enum NotificationType
+    {
+        Welcome = 1,
+        OrderConfirmation = 2
+    }
+}
