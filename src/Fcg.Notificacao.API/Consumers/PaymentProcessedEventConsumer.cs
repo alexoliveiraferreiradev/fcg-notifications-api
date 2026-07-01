5befbc1171ed4fb406da550486a3efd46e2dfc8c
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-01 17:13:31 -0300

    refactor: ajustar consumidor para usar PaymentProcessedEvent e validar status de aprovacao

--- src/Fcg.Notificacao.API/Consumers/PaymentProcessedEventConsumer.cs
+++ src/Fcg.Notificacao.API/Consumers/PaymentProcessedEventConsumer.cs
@@ -1,10 +1,11 @@
-﻿using Fcg.Core.Abstractions.MessageContracts;
+﻿using Fcg.Core.Abstractions.Enum;
+using Fcg.Core.Abstractions.MessageContracts;
 using Fcg.Notificacao.Application.UseCase.ApprovedPaymentEmail;
 using MassTransit;
 
 namespace Fcg.Notificacao.API.Consumers
 {
-    public class PaymentProcessedEventConsumer : IConsumer<OrderApprovedEvent>
+    public class PaymentProcessedEventConsumer : IConsumer<PaymentProcessedEvent>
     {
         private readonly SendPaymentApprovedEmailUseCase _useCase;
 
@@ -13,13 +14,16 @@ namespace Fcg.Notificacao.API.Consumers
             _useCase = useCase; 
         }
 
-        public async Task Consume(ConsumeContext<OrderApprovedEvent> context)
+        public async Task Consume(ConsumeContext<PaymentProcessedEvent> context)
         {
             var mensagem = context.Message;
 
+            if (mensagem.Status != PaymentStatus.Approved)
+                return;
+
             var command = new SendPaymentApprovedEmailCommand(context.MessageId ?? Guid.NewGuid(),UsuarioId: mensagem.UserId,
                 OrderId: mensagem.OrderId, NomeUsuario: mensagem.NomeUsuario, Email: mensagem.EmailUsuario,
-                DataAquisicao: mensagem.DataCompra);
+                DataAquisicao: mensagem.CreatedAt);
 
             await _useCase.ExecuteAsync(command,context.CancellationToken);
         }
