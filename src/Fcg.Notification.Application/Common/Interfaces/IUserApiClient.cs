7a2ec9f228c88e5671472338c948710c13716d02
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-09 18:24:45 -0300

    refactor(application): remover client HTTP UserApiClient e sua respectiva interface

--- src/Fcg.Notification.Application/Common/Interfaces/IUserApiClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using Fcg.Notification.Application.Models;
-
-namespace Fcg.Notification.Application.Common.Interfaces
-{
-    public interface IUserApiClient
-    {
-        Task<UserProfileCacheModel?> GetProfileAsync(Guid userId, CancellationToken cancellationToken = default);
-    }
-}
